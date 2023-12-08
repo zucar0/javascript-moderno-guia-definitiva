604faa431848c9e6380866f75656b4b9e4eef64e
Antonio <antonio.enralv@gmail.com>
2023-12-08 10:17:28 -0700

    Sección 1. Introducción

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,11 @@
+console.log('Hola');
+console.log('Mundo');
+
+function hola(){
+    console.log('Hola');
+    console.log('Mundo');
+    console.log('ok');
+}
+//const nombre = prompt("¿Cuál es tu nombre");
+//document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+//Video 7: alert("Hola mundo")
