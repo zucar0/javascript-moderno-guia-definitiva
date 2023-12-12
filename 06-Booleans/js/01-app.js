06dce99a59673226bacf86b2c0810e38724d3447
Antonio <antonio.enralv@gmail.com>
2023-12-12 08:32:43 -0700

    Sección 6: Booleans en JavaScript

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,22 @@
+const boolean1 = true;
+const boolean2 = false; 
+const boolean3 = "true";
+const boolean4 = new Boolean(true);
+
+
+// Ver en consola
+console.log('boolean1: ', boolean1);
+console.log('boolean2: ', boolean2);
+console.log('boolean3: ', boolean3);
+console.log('boolean4: ', boolean4);
+
+// Saber el tipo de dato
+console.log('Type of boolean1: ', typeof boolean1);
+console.log('Type of boolean2: ', typeof boolean2);
+console.log('Type of boolean3: ', typeof boolean3);
+console.log('Type of boolean4: ', typeof boolean4);
+
+// Comparación
+console.log('boolean1 === boolean3: ', boolean1 === boolean3);
+
+// Constructor de objeto Boolean
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,10 @@
+const autenticado = true; 
+if(autenticado === true){
+    console.log('autenticado: Si puedes ver Netflix')
+}else{
+    console.log('autenticado: No, no puedes verlo')
+}
+
+// Operador ternario
+const autenticadoternario = true;
+console.log('autenticadoternario: ', autenticadoternario ? 'Sí está autenticado' : 'No está autenticado');
